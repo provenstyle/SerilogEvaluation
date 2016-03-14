83edfe6b815ecefa3b86f5d0c2938836052c7546
michael dudley <michael.dudley@improvingenterprises.com>
2016-03-14 08:01:13 -0500

    Integrated with the Castle logging facility

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,42 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Serilog;
-using Serilog.Sinks.MSSqlServer;
-using Serilog.Configuration;
 using System.IO;
+using Castle.Facilities.Logging;
+using Castle.Services.Logging.SerilogIntegration;
+using Castle.Windsor;
+using Serilog;
 
-namespace ConsoleApplication1
+namespace SerilogEvaluation
 {
     class Program
     {
-        private static ILogger logger;
+        private static Castle.Core.Logging.ILogger logger;
 
         static void Main(string[] args)
         {
-            var connectionString = "Data Source=.;Initial Catalog=SFAppLogs;Integrated Security=True;App=BenefitsAdmin";
-            var table = "Logs";
-            var columnOptions = new ColumnOptions
-            {
-                AdditionalDataColumns = new List<DataColumn>(),
-                Level = {StoreAsEnum = false},
-                TimeStamp = { ConvertToUtc = false},
-                Properties = { },
-                Store = new List<StandardColumn>()
-            };
+            var config = new LoggerConfiguration()
+                .ReadFrom.AppSettings();
 
-            logger = new LoggerConfiguration()
-                .ReadFrom.AppSettings()
-                .WriteTo.MSSqlServer(connectionString, table, autoCreateSqlTable: true)
-                //.WriteTo.Console()
-                .CreateLogger();
+            var container = new WindsorContainer();
+            container.AddFacility<LoggingFacility>(f => f.LogUsing(new SerilogFactory(config)));
+            logger = container.Resolve<Castle.Core.Logging.ILogger>(); 
 
             Serilog.Debugging.SelfLog.Out = new StreamWriter("c:\\temp\\serilog.log");
             Serilog.Debugging.SelfLog.WriteLine("***From the selflog");
+
             logger.Debug("Foo");
+            logger.DebugFormat("My message {@TestData}", new {Foo = "Foo data", Bar = "Bar value"});
+            logger.DebugFormat("My object {@TestData}", new Fizz("asdf"));
+            logger.DebugFormat("My object {}", new Fizz("asdf"));
+            logger.Error("Testing errors", new ArgumentException("Test"));
       }
+    }
+
+    public class Fizz
+    {
+        public Fizz(string value)
+        {
+            Buzz = value;
+        }
+        public string Buzz { get; set; }
     }
 }
