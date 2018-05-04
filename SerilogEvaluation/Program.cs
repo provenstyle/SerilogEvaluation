ba00d8b2746e5bac1993a779d5f7f4b85fe26aec
michael dudley <michael.dudley@improvingenterprises.com>
2018-05-04 15:48:33 -0500

    adding an mssql sink with custom column names

--- SerilogEvaluation/Program.cs
+++ SerilogEvaluation/Program.cs
@@ -1,34 +1,55 @@
-﻿using System;
-using System.IO;
-using Castle.Facilities.Logging;
-using Castle.Services.Logging.SerilogIntegration;
-using Castle.Windsor;
-using Serilog;
-
-namespace SerilogEvaluation
+﻿namespace SerilogEvaluation
 {
+    using System;
+    using System.IO;
+    using Castle.Facilities.Logging;
+    using Castle.Services.Logging.SerilogIntegration;
+    using Castle.Windsor;
+    using Serilog;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Reflection;
+    using Serilog.Debugging;
+    using Serilog.Sinks.MSSqlServer;
+
     class Program
     {
         private static Castle.Core.Logging.ILogger logger;
 
         static void Main(string[] args)
         {
+            //Serilog Internal logging
+            SelfLog.Enable(Console.Error);
+            //SelfLog.Enable(new StreamWriter("c:\\temp\\serilog.log"));
+
+            var columnOptions = new ColumnOptions();
+            columnOptions.Level.ColumnName      = "LogLevel";
+            columnOptions.Message.ColumnName    = "LogMessage";
+            columnOptions.TimeStamp.ColumnName  = "LogTimeStamp";
+            columnOptions.AdditionalDataColumns = new List<DataColumn> {
+                new DataColumn("ApplicationName", typeof(string))
+            };
+
             var config = new LoggerConfiguration()
-                .ReadFrom.AppSettings();
+                .Enrich.WithProperty("ApplicationName", Assembly.GetExecutingAssembly().GetName().Name)
+                .WriteTo.MSSqlServer("Serilog", "Logs", columnOptions:columnOptions)
+                .ReadFrom.AppSettings()
+                .CreateLogger();
 
             var container = new WindsorContainer();
             container.AddFacility<LoggingFacility>(f => f.LogUsing(new SerilogFactory(config)));
-            logger = container.Resolve<Castle.Core.Logging.ILogger>(); 
+            logger = container.Resolve<Castle.Core.Logging.ILogger>();
 
-            Serilog.Debugging.SelfLog.Out = new StreamWriter("c:\\temp\\serilog.log");
-            Serilog.Debugging.SelfLog.WriteLine("***From the selflog");
-
+            config.Verbose("This is Verbose");
             logger.Debug("Foo");
             logger.DebugFormat("My message {@TestData}", new {Foo = "Foo data", Bar = "Bar value"});
             logger.DebugFormat("My object {@TestData}", new Fizz("asdf"));
             logger.DebugFormat("My object {}", new Fizz("asdf"));
+            logger.Warn("This is a warning.");
             logger.Error("Testing errors", new ArgumentException("Test"));
-      }
+
+            config.Dispose(); //Calling dispose here just to make sure serilog flushes the batch before we exit
+        }
     }
 
     public class Fizz
---
+++ SerilogEvaluation/logs.sql
@@ -0,0 +1,22 @@
+﻿SET ANSI_NULLS ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+
+IF OBJECT_ID(N'dbo.Logs', N'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.Logs (
+       Id              INT IDENTITY(1,1) NOT NULL,
+       LogMessage      NVARCHAR(MAX)     NULL,
+       MessageTemplate NVARCHAR(MAX)     NULL,
+       LogLevel        NVARCHAR(128)     NULL,
+       LogTimeStamp    DATETIMEOFFSET(7) NOT NULL,
+       Exception       NVARCHAR(MAX)     NULL,
+       Properties      XML               NULL,
+       LogEvent        NVARCHAR(MAX)     NULL,
+       ApplicationName NVARCHAR(128)     NULL
+
+       CONSTRAINT PK_Logs PRIMARY KEY CLUSTERED(Id ASC)
+    )
+
+END;
